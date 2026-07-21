ecf569136aa04bcdd760129488551d34ca0439ce
TPScode <thomasspier99@gmail.com>
2026-07-22 01:45:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mexo-v50";
+const CACHE = "mexo-v51";
 const KERN = ["./", "./index.html", "./config.js", "./manifest.json", "./icon-192.png", "./icon-512.png", "./icon-maskable-512.png"];
 
 // Robuust: cache elk bestand afzonderlijk, zodat één ontbrekend bestand
